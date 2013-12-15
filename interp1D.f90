98e811a0ffbd1a465c95d35de64d2bc0a159e507
Alexander Robinson <robinson@fis.ucm.es>
2013-12-14 18:09:11 -0800

    Added 1D interpolation module to coord package.

---
+++ interp1D.f90
@@ -0,0 +1,263 @@
+
+module interp1D
+
+    implicit none 
+
+contains
+
+
+    function interp_spline(x,y,xout) result(yout)
+
+        implicit none 
+
+        integer :: nout 
+        double precision, dimension(:) :: x, y
+        double precision, dimension(:) :: xout
+        double precision, dimension(size(xout)) :: yout 
+        double precision, dimension(:), allocatable :: b, c, d 
+        double precision :: uh, dx, yh  
+        integer :: i, n 
+
+        n    = size(x) 
+        nout = size(xout)
+
+        ! Get spline coefficients b, c, d
+        allocate(b(n),c(n),d(n))
+        call spline (x, y, b, c, d, n)
+
+        do i = 1, nout 
+            if (xout(i) .lt. x(1)) then
+                dx = x(1)-xout(i)
+                uh = x(1)+dx
+                yh = ispline(uh,x,y,b,c,d,n)
+                yout(i) = y(1) + (y(1)-yh)
+                !write(*,*) x(1), xout(i), dx, uh, y(1), yh, yout(i)
+            else if (xout(i) .gt. x(n)) then
+                dx = xout(i)-x(n)
+                uh = x(n)-dx
+                yh = ispline(uh,x,y,b,c,d,n)
+                yout(i) = y(n) + (y(n)-yh)
+                !write(*,*) x(n), xout(i), dx, uh, y(n), yh, yout(i)
+            else
+                yout(i) = ispline(xout(i), x, y, b, c, d, n)
+            end if 
+        end do 
+
+        return
+
+    end function interp_spline 
+
+!     function ispline_outer(u, x, y, b, c, d, n) result(yout)
+
+!         implicit none 
+
+!         integer :: n
+!         double precision ::  u, x(n), y(n), b(n), c(n), d(n)
+!         double precision :: uh, dx, yh  
+
+!         if (u .lt. x(1)) then 
+!             dx = x(1)-u
+!             uh = x(1) + dx 
+!             yh = ispline(uh,x,y,b,c,d,n)
+!             yout = y(1) + (yh-y(1))
+!         else if (u .gt. x(n)) then
+!             dx = u-x(n)
+!             uh = x(n)-dx 
+!             yh = ispline(uh,x,y,b,c,d,n)
+!             yout = y(n) + (y(n)-yh)
+!         end if 
+
+!         return
+
+!     end function ispline_outer 
+
+
+   subroutine spline (x, y, b, c, d, n)
+!======================================================================
+!  SOURCE: http://ww2.odu.edu/~agodunov/computing/programs/book2/Ch01/spline.f90
+!  Calculate the coefficients b(i), c(i), and d(i), i=1,2,...,n
+!  for cubic spline interpolation
+!  s(x) = y(i) + b(i)*(x-x(i)) + c(i)*(x-x(i))**2 + d(i)*(x-x(i))**3
+!  for  x(i) <= x <= x(i+1)
+!  Alex G: January 2010
+!----------------------------------------------------------------------
+!  input..
+!  x = the arrays of data abscissas (in strictly increasing order)
+!  y = the arrays of data ordinates
+!  n = size of the arrays xi() and yi() (n>=2)
+!  output..
+!  b, c, d  = arrays of spline coefficients
+!  comments ...
+!  spline.f90 program is based on fortran version of program spline.f
+!  the accompanying function fspline can be used for interpolation
+!======================================================================
+implicit none
+integer n
+double precision, dimension(:) :: x, y, b, c, d 
+!double precision x(n), y(n), b(n), c(n), d(n)
+integer i, j, gap
+double precision h
+
+gap = n-1
+! check input
+if ( n < 2 ) return
+if ( n < 3 ) then
+  b(1) = (y(2)-y(1))/(x(2)-x(1))   ! linear interpolation
+  c(1) = 0.
+  d(1) = 0.
+  b(2) = b(1)
+  c(2) = 0.
+  d(2) = 0.
+  return
+end if
+!
+! step 1: preparation
+!
+d(1) = x(2) - x(1)
+c(2) = (y(2) - y(1))/d(1)
+do i = 2, gap
+  d(i) = x(i+1) - x(i)
+  b(i) = 2.0*(d(i-1) + d(i))
+  c(i+1) = (y(i+1) - y(i))/d(i)
+  c(i) = c(i+1) - c(i)
+end do
+!
+! step 2: end conditions 
+!
+b(1) = -d(1)
+b(n) = -d(n-1)
+c(1) = 0.0
+c(n) = 0.0
+if(n /= 3) then
+  c(1) = c(3)/(x(4)-x(2)) - c(2)/(x(3)-x(1))
+  c(n) = c(n-1)/(x(n)-x(n-2)) - c(n-2)/(x(n-1)-x(n-3))
+  c(1) = c(1)*d(1)**2/(x(4)-x(1))
+  c(n) = -c(n)*d(n-1)**2/(x(n)-x(n-3))
+end if
+!
+! step 3: forward elimination 
+!
+do i = 2, n
+  h = d(i-1)/b(i-1)
+  b(i) = b(i) - h*d(i-1)
+  c(i) = c(i) - h*c(i-1)
+end do
+!
+! step 4: back substitution
+!
+c(n) = c(n)/b(n)
+do j = 1, gap
+  i = n-j
+  c(i) = (c(i) - d(i)*c(i+1))/b(i)
+end do
+!
+! step 5: compute spline coefficients
+!
+b(n) = (y(n) - y(gap))/d(gap) + d(gap)*(c(gap) + 2.0*c(n))
+do i = 1, gap
+  b(i) = (y(i+1) - y(i))/d(i) - d(i)*(c(i+1) + 2.0*c(i))
+  d(i) = (c(i+1) - c(i))/d(i)
+  c(i) = 3.*c(i)
+end do
+c(n) = 3.0*c(n)
+d(n) = d(n-1)
+end subroutine spline
+
+  function ispline(u, x, y, b, c, d, n)
+!======================================================================
+! SOURCE: http://ww2.odu.edu/~agodunov/computing/programs/book2/Ch01/spline.f90
+! function ispline evaluates the cubic spline interpolation at point z
+! ispline = y(i)+b(i)*(u-x(i))+c(i)*(u-x(i))**2+d(i)*(u-x(i))**3
+! where  x(i) <= u <= x(i+1)
+!----------------------------------------------------------------------
+! input..
+! u       = the abscissa at which the spline is to be evaluated
+! x, y    = the arrays of given data points
+! b, c, d = arrays of spline coefficients computed by spline
+! n       = the number of data points
+! output:
+! ispline = interpolated value at point u
+!=======================================================================
+implicit none
+double precision ispline
+integer n
+! double precision  u, x(n), y(n), b(n), c(n), d(n)
+double precision :: u 
+double precision, dimension(:) :: x, y, b, c, d 
+integer i, j, k
+double precision dx
+
+! if u is ouside the x() interval take a boundary value (left or right)
+if(u <= x(1)) then
+  ispline = y(1)
+  return
+end if
+if(u >= x(n)) then
+  ispline = y(n)
+  return
+end if
+
+!*
+!  binary search for for i, such that x(i) <= u <= x(i+1)
+!*
+i = 1
+j = n+1
+do while (j > i+1)
+  k = (i+j)/2
+  if(u < x(k)) then
+    j=k
+    else
+    i=k
+   end if
+end do
+!*
+!  evaluate spline interpolation
+!*
+dx = u - x(i)
+ispline = y(i) + dx*(b(i) + dx*(c(i) + dx*d(i)))
+end function ispline
+
+
+end module interp1D
+
+
+! program test
+
+!     use interp1D 
+
+!     implicit none 
+
+!     double precision, parameter :: pi = 2.d0*acos(0.d0)
+
+!     integer, parameter :: n    = 12
+!     integer, parameter :: nout = 360
+    
+!     double precision, dimension(n)    :: x, y
+!     double precision, dimension(nout) :: xout, yout
+
+!     integer :: k, q 
+
+!     do k = 1, n 
+!         x(k) = k*30-15 
+!         y(k) = sin(x(k)*2*pi/dble(nout)) + (k-6)*0.3d0
+!     end do 
+
+!     do k = 1, nout 
+!         xout(k) = k 
+!     end do 
+
+!     yout = interp_spline(x,y,xout)
+
+!     q = 1 
+!     do k = 1, nout 
+        
+!         if (xout(k) .eq. x(q)) then 
+!             write(*,"(2f10.3,2f10.3)") x(q), y(q), xout(k), yout(k) 
+!             q = q+1
+!         else
+!             write(*,"(2a10,2f10.3)") "NA","NA", xout(k), yout(k) 
+!         end if 
+
+!     end do 
+
+! end program test 
