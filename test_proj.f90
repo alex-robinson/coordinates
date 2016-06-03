9a015f73a62b932388cb81282e04d1fe3dd412c3
Alexander Robinson <robinson@fis.ucm.es>
2016-06-03 15:23:50 +0200

    Added source code to calculate corners of Bamber polar stereographic projection for testing.

---
+++ test_proj.f90
@@ -0,0 +1,22 @@
+
+
+program test_proj 
+
+    use coordinates 
+
+    implicit none 
+
+    type(points_class) :: pts 
+
+    integer :: i 
+
+    call points_init(pts,name="Bamber-corners",mtype="polar_stereographic",units="kilometers", &
+                     lambda=-39.d0,phi=90.d0,alpha=19.d0,lon180=.TRUE., &
+                     x=[-800.d0,-800.d0,700.d0,700.d0],y=[-3400.d0,-600.d0,-600.d0,-3400.d0])
+
+
+    do i = 1, size(pts%x,1)
+        write(*,"(2f10.3,a,2f10.3)") pts%x(i), pts%y(i), " => ", pts%lat(i), pts%lon(i)
+    end do 
+
+end program test_proj
