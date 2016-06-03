cdd1cfa572d0aab6eb7ac5499c3de23b3006a779
Alexander Robinson <robinson@fis.ucm.es>
2016-06-03 17:37:43 +0200

    Added src file.

---
+++ proj_etopo1.f90
@@ -0,0 +1,160 @@
+
+program test_etopo
+
+    use ncio 
+    use coordinates
+
+    implicit none
+
+    type(grid_class) :: grid0, grid1, grid2, grid3, grid4
+    double precision, allocatable :: var0(:,:), var1(:,:) 
+    double precision :: tmplon(720), tmplat(360)
+    
+    character(len=256) :: file0
+    double precision :: table(4,4)
+    integer :: i 
+
+    write(*,*) 
+    
+    ! =======================================================================
+    !
+    ! Define global input grid and load input data
+    !
+    ! =======================================================================
+
+    file0  = "data/ETOPO1-ICE-050deg.nc"
+
+    ! Define input latlon grid
+    call nc_read(file0,"lon",tmplon)
+    call nc_read(file0,"lat",tmplat)
+    call grid_init(grid0,name="GLOBAL-050deg",mtype="latlon",units="degrees",lon180=.TRUE., &
+                     x=tmplon,y=tmplat)
+
+    ! Allocate arrays of the size of the global grid
+    call grid_allocate(grid0, var0)
+
+    ! Load original data
+    call nc_read(file0,"z",var0)
+
+    ! =========================================================
+    !
+    ! PROJECTED GRID DEFINITIONS
+    !
+    ! =========================================================
+
+    call grid_init(grid1,name="ANT-40KM",mtype="polar_stereographic",units="kilometers", &
+                   lon180=.TRUE.,dx=40.d0,nx=156,dy=40.d0,ny=146, &
+                   lambda=0.d0,phi=-90.d0,alpha=22.1d0)
+
+    call grid_init(grid2,name="ANT-20KM",mtype="polar_stereographic",units="kilometers", &
+                   lon180=.TRUE.,dx=20.d0,nx=311,dy=20.d0,ny=291, &
+                   lambda=0.d0,phi=-90.d0,alpha=22.1d0)
+
+    call grid_init(grid3,name="ANT-10KM",mtype="polar_stereographic",units="kilometers", &
+                   lon180=.TRUE.,dx=10.d0,nx=621,dy=10.d0,ny=581, &
+                   lambda=0.d0,phi=-90.d0,alpha=22.1d0)
+
+    call grid_init(grid4,name="ANT-5KM",mtype="polar_stereographic",units="kilometers", &
+                   lon180=.TRUE.,dx=10.d0,nx=1241,dy=10.d0,ny=1161, &
+                   lambda=0.d0,phi=-90.d0,alpha=22.1d0)
+
+
+    ! =========================================================
+    !
+    ! TEST MAPPING SPEEDS
+    !
+    ! =========================================================
+    table = 0.d0 
+
+    table(1,:) = test_mapping(var0,grid0,grid1,niter=100)
+    table(2,:) = test_mapping(var0,grid0,grid2,niter=100)
+    table(3,:) = test_mapping(var0,grid0,grid3,niter=100)
+    table(4,:) = test_mapping(var0,grid0,grid4,niter=100)
+
+    write(*,*)
+    write(*,*) "Mapping/interpolation summary"
+    do i = 1, 4 
+        write(*,"(f10.1,f10.3,f5.1,f10.5)") table(i,:)
+    end do 
+
+
+    write(*,*) 
+    write(*,*) "Done!"
+    write(*,*) 
+
+contains
+
+    function test_mapping(var0,grid0,grid1,niter) result(table)
+
+        implicit none 
+
+        double precision :: var0(:,:)
+        type(grid_class) :: grid0, grid1
+        integer          :: niter 
+        double precision :: table(4)
+
+        ! Local variables
+        type(map_class)    :: map
+        double precision, allocatable :: var1(:,:)
+        integer,          allocatable :: mask1(:,:)
+
+        double precision   :: start, finish 
+        double precision   :: mapping_time, interp_time 
+        character(len=256) :: file1
+        integer            :: i 
+
+        ! Allocate output arrays 
+        call grid_allocate(grid1,var1)
+        call grid_allocate(grid1,mask1)
+        
+        ! First, perform mapping once, record time
+        write(*,*) "" 
+        write(*,*) "Mapping ..."
+        write(*,*) "" 
+
+        call cpu_time(start)
+
+        ! Create a map object for grid0=>grid1 mapping
+        call map_init(map,grid0,grid1,max_neighbors=10,lat_lim=2.0d0,fldr="maps",load=.FALSE.)
+    
+        call cpu_time(finish)
+        mapping_time = (finish-start)/60.0d0
+
+
+        ! Next perform interpolation niter times
+        write(*,*) "" 
+        write(*,*) "Interpolating ..."
+        write(*,*) "" 
+
+        call cpu_time(start)
+        
+        do i = 1, niter 
+
+            call map_field(map,"zs",var0,var1,mask1,method="radius")
+
+        end do 
+
+        call cpu_time(finish)
+        interp_time = (finish-start)/60.0d0
+
+        write(*,*) "Summary: "//trim(map%name1)//" => "//trim(map%name2)
+        write(*,"(a,f7.2,i4,f7.5)") "Mapping time, niter, interp. time (min.): ", &
+                                         mapping_time, niter, interp_time 
+
+        ! Write results to netcdf file 
+        call map_field(map,"zs",var0,var1,mask1,method="radius")
+
+        file1  = "output/"//trim(grid1%name)//"_ETOPO1-ICE-050deg.nc"
+        call grid_write(grid1,file1,xnm="xc",ynm="yc",create=.TRUE.)
+        call nc_write(file1,"zs",var1,dim1="xc",dim2="yc")
+
+        table = [dble(grid1%npts),mapping_time,dble(niter),interp_time]
+
+        return 
+
+
+    end function test_mapping 
+
+
+end program test_etopo
+
